623d40a4b8e15bdb2d3ff10ba5bbf6c534967ab4
Astakz <asadbek888888@gmail.com>
2025-10-30 00:54:58 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/ZMZ-DOKUMENT/", // 👈 МІНЕ ОСЫ ЖАҢА ЖОЛ ҚОСЫЛДЫ
   server: {
     host: "::",
     port: 8080,
